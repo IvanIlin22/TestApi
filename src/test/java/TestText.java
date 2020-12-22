815d60b5cfeb9c8ccb08ebdb7582225519b3eb29
Ivan Ilin <geniusmov@yandex.ru>
2020-12-22 08:32:36 +0300

    renamed file and added description

--- src/test/java/TestText.java
+++ src/test/java/TestText.java
@@ -10,7 +10,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class testText {
+public class TestText {
 	String urlVacancy="https://api.hh.ru/vacancies";
 	
 	@Test
